b29b0b05dd2ee8032b33bfa37add7152c4ab692f
Kel <tyresemaynard@gmail.com>
2023-04-10 16:28:55 -0400

    rental page functionality

---
+++ php/get_vehicle_id.php
@@ -0,0 +1,28 @@
+<?php
+// establish a database connection
+$conn = mysqli_connect('localhost', 'root', '', 'rental_system');
+
+// check for connection errors
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$license_plate = $_POST['license_plate'];
+
+$query = "SELECT id FROM vehicles WHERE license_plate_number = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("s", $license_plate);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode(['id' => $row['id']]);
+} else {
+    echo json_encode(['id' => -1]);
+}
+
+$stmt->close();
+$conn->close();
+?>
+
