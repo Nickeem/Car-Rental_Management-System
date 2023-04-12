40ba59f5e04754302942f62ac616d174dc916f69
Kel <tyresemaynard@gmail.com>
2023-04-12 02:36:19 -0400

    reports page functionality -wip

---
+++ php/customer_rental.php
@@ -0,0 +1,47 @@
+<?php
+//for generating customer rental history report
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+// Replace the following values with your own database credentials
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "rental_system";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the customer's driver's license number from the request
+$license = $_POST['license'];
+
+// Query the database for the customer rental history
+$sql = "SELECT rentals.id, rentals.rental_start_date, rentals.rental_end_date, rentals.rental_rate, rentals.additional_charges, vehicles.make, vehicles.model
+        FROM rentals
+        INNER JOIN customers ON rentals.customer_id = customers.id
+        INNER JOIN vehicles ON rentals.vehicle_id = vehicles.id
+        WHERE customers.driver_license_number = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $license);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+// Return the data as JSON
+echo json_encode($data);
+
+// Close the connection
+$conn->close();
+?>
+
