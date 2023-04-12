69ba752c515fc553649f7178d0692cd49863b143
Nickeem <vacation1478@gmail.com>
2023-04-12 01:24:37 -0400

    added rental logic

--- php/fetch_license_plates.php
+++ php/fetch_license_plates.php
@@ -6,8 +6,13 @@ if (!$conn) {
 }
 
 $pattern = $_POST['pattern'];
+$return_rented = $_POST['return_rented'];
+$availability = "not-rented";
+if($return_rented == "true") { // vehicles returned are dependent on form type and whether vehicles are rented or not rented
+    $availability = "rented";
+}
 
-$query = "SELECT * FROM vehicles WHERE license_plate_number LIKE '%$pattern%' AND availability = 'not-rented'";
+$query = "SELECT * FROM vehicles WHERE license_plate_number LIKE '%$pattern%' AND availability = '$availability'";
 $result = mysqli_query($conn, $query);
 
 $data = array();
