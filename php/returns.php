071bc1e3189810ffd0f4440d3244767dea40e4b1
Nickeem <vacation1478@gmail.com>
2023-04-12 10:25:34 -0400

    commented rogue if statement

--- php/returns.php
+++ php/returns.php
@@ -37,11 +37,11 @@ $result = mysqli_query($conn, $update_query);
 $return_query = "UPDATE rentals SET actual_return_date = $actual_return_date, late_fee = $late_fee, additional_charges = $additional_charges, rental_status = 'completed' WHERE vehicle_id = $vehicle_id AND customer_id = $customer_id;";
 $return_result = mysqli_query($conn, $return_query);
 
-if($_POST['new-damage'] != '') {
+//if($_POST['new-damage'] != '') {
     // update vehicle damge in vehicles table
     // update vehicle damage in rentals table - should be a field for damage before and damage after
 
-    }
+  //  }
 
 
 if ($return_result) {
